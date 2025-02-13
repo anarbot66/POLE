c7429cc624d34a9de02161fd4667269075b29cbd
anarbot66 <thehoolystar@gmail.com>
2025-02-13 17:07:49 +0300

    Update bot.js

--- src/server/bot.js
+++ src/server/bot.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot('8117729038:AAF0Ue7su9aYK9-RzTrb1sljHKhj99D5iHI', { polling: true });
+const bot = new TelegramBot( , { polling: true });
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
